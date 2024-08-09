9293a227d49c0e03cc6e263ab6f7d858bba16222
Felipe Iasi <felipeiasi@gmail.com>
2024-08-09 12:22:24 +0200

    Test 1 component only

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,7 @@ const App: React.FC = () => {
       <div>
         <h1>MQTT React App</h1>
         <MessageInput topic="vite/react2" />
-        <MessageInput topic="vite/react1" />
         <MessageList topic="vite/react2" />
-        <MessageList topic="vite/react1" />
       </div>
     </MqttProvider>
   );
